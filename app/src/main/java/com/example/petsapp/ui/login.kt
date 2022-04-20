d313e5d72de7e6398465f3ac9027c5b0706c591a
RichieVRich <55894654+RichieVRich@users.noreply.github.com>
2022-04-19 17:59:08 -0700

    New content

---
+++ app/src/main/java/com/example/petsapp/ui/login.kt
@@ -0,0 +1,12 @@
+package com.example.petsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.petsapp.R
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
