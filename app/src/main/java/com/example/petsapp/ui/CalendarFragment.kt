5194e7098bfc101a83449363fe52d36dd90c9031
RichieVRich <55894654+RichieVRich@users.noreply.github.com>
2022-05-28 13:54:21 -0700

    baloney

--- app/src/main/java/com/example/petsapp/ui/CalendarFragment.kt
+++ app/src/main/java/com/example/petsapp/ui/CalendarFragment.kt
@@ -1,12 +1,19 @@
 package com.example.petsapp.ui
 
+import android.icu.util.Calendar
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.petsapp.R
 
 class CalendarFragment : Fragment(R.layout.calendar) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        var rightnow = Calendar.getInstance()
+
     }
 }
